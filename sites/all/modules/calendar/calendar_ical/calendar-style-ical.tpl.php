186bd01eea5698fb32f2cf9a03fc5cbce8925988
mattpclark <web.development@undp.org>
2011-09-30 14:58:37 +0700

    add new modules

--- sites/all/modules/calendar/calendar_ical/calendar-style-ical.tpl.php
+++ sites/all/modules/calendar/calendar_ical/calendar-style-ical.tpl.php
@@ -3,12 +3,16 @@
  * $title
  *   The name of the calendar.
  */
-
+if (empty($method)) {
+  $method = 'PUBLISH';
+}
 ?>
 BEGIN:VCALENDAR
 VERSION:2.0
-METHOD:PUBLISH
-X-WR-CALNAME: <?php print $title . "\r\n"; ?>
+METHOD:<?php print $method; ?>
+<?php if (!empty($calname)): ?>
+X-WR-CALNAME;VALUE=TEXT:<?php print $calname . "\r\n"; ?>
+<?php endif; ?>
 PRODID:-//Drupal iCal API//EN
 <?php print $rows ?>
 END:VCALENDAR
